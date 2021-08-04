a8255a7695576fb428a94c975b29599262b02d7c
DESKTOP-RIGDD5R\minrn <minrnullpo@gmail.com>
2021-08-04 09:40:28 +0700

    checkpoint create handling bayar at dashboard kasir

--- src/components/kasir-components/list-pesanan-table-components/pembayaran-modal/pembayaran-modal.component.jsx
+++ src/components/kasir-components/list-pesanan-table-components/pembayaran-modal/pembayaran-modal.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./pembayaran-modal.styles.scss";
+import axios from "axios";
 
 // Handling Redux
 import { useSelector, useDispatch } from "react-redux";
@@ -10,7 +11,7 @@ import {
 } from "../../../../redux/listPesanan/listPesanan.action";
 import { removeDetailPesanan } from "../../../../redux/detailPesanan/detailPesanan.action";
 import { selectInfoPesanan } from "../../../../redux/detailPesanan/detailPesanan.selectors";
-
+import { selectCurrentUser } from "../../../../redux/user/user.selectors";
 // Import Component
 import { Modal, Skeleton, Button, Space, InputNumber, message } from "antd";
 
@@ -19,33 +20,53 @@ const PembayaranModal = () => {
   const [nominal, setNominal] = useState(null);
   const isModalHIdden = useSelector(selectModalBayarHidden);
   const dataPemesan = useSelector(selectInfoPesanan);
+  const { IdUser } = useSelector(selectCurrentUser);
 
   useEffect(() => {
     if (isModalHIdden === true && nominal) setNominal(null);
   }, [isModalHIdden]);
+
   const handlingModalOnCancel = () => {
     dispatch(removeDetailPesanan());
     dispatch(removeDataPesanan());
     dispatch(toggleModalBayarHidden());
   };
 
-  const handlingDisableBtnBayar = () => {};
-
   const numberFormatRupiah = numb => {
     return numb.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   };
+
   const handlingNominalChange = value => {
     setNominal(value);
+  };
+
+  const handlingAddPembayaran = () => {
+    const { IdPesanan, TotalHarga } = dataPemesan;
+    const Kembalian = nominal - TotalHarga;
+    axios
+      .post("/pembayaran/add", {
+        IdPesanan,
+        IdUser,
+        Nominal: nominal,
+        Kembalian,
+      })
+      .then(response => {
+        dispatch(removeDetailPesanan());
+        dispatch(removeDataPesanan());
+        dispatch(toggleModalBayarHidden());
+        message.success("Pembayaran Berhasil!");
+      })
+      .catch(error => {
+        console.log(error);
+        message.error("Pembayaran Gagal!");
+      });
   };
   const onBayarClick = () => {
     if (nominal) {
       if (nominal < dataPemesan.TotalHarga)
         message.error("Nominal kurang dari total harga");
-      else {
-      }
-    } else {
-      message.error("Nominal Tidak Boleh Kosong!");
-    }
+      else handlingAddPembayaran();
+    } else message.error("Nominal Tidak Boleh Kosong!");
   };
   return (
     <Modal
@@ -102,7 +123,7 @@ const PembayaranModal = () => {
           <Button
             block
             className="btn-action-secondary"
-            disabled={dataPemesan ? handlingDisableBtnBayar() : true}
+            disabled={dataPemesan ? false : true}
             onClick={onBayarClick}
           >
             Bayar
